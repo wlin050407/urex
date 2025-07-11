6c1694e565fd0c221e96292383e107bcbb73b92f
wlin050407 <wlin05@u.nus.edu>
2025-07-11 21:31:31 +0800

    fix: set Vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/urex/', // 适配GitHub Pages
   plugins: [react()],
   server: {
     port: 3000,
